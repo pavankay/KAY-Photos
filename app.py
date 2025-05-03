e058e620b6a8665466e8185a10cbe589848f6033
pavankay <pavankay@gmail.com>
2025-05-02 17:44:10 -0700

    Add CORS support to Flask app and update requirements.txt

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template, send_file, Response, send_from_directory
 import os
+from flask_cors import CORS
 
 app = Flask(__name__)
+
+CORS(app)
 
 def get_content_type(filename):
     if filename.endswith('.js.br'):
